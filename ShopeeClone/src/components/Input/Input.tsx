b793247d795284bddcdb0330e7007b66ada6493a
huanhutlinh <huanhutlinh128@gmail.com>
2022-12-09 23:04:13 +0700

    Tạo component Input để rút gọn code

---
+++ ShopeeClone/src/components/Input/Input.tsx
@@ -0,0 +1,36 @@
+import { RegisterOptions, UseFormRegister } from 'react-hook-form'
+
+interface Props {
+  type: React.HTMLInputTypeAttribute
+  errorMessage?: string
+  placeholder?: string
+  className?: string
+  name: string
+  register: UseFormRegister<any>
+  rules?: RegisterOptions
+  autoComplete?: string
+}
+
+export default function Input({
+  className,
+  type,
+  placeholder,
+  autoComplete,
+  register,
+  name,
+  rules,
+  errorMessage
+}: Props) {
+  return (
+    <div className={className}>
+      <input
+        type={type}
+        className='p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-sm'
+        placeholder={placeholder}
+        autoComplete={autoComplete}
+        {...register(name, rules)}
+      />
+      <div className='mt-1 text-red-600 min-h-[1.25rem] text-sm'>{errorMessage}</div>
+    </div>
+  )
+}
